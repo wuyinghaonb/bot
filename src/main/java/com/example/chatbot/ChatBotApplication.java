97544c339f0c22e912b1ca2b5ab9308ffde3c70a
wyh <417689923@qq.com>
2024-04-01 01:30:49 +0800

    禁用tomcat

--- src/main/java/com/example/chatbot/ChatBotApplication.java
+++ src/main/java/com/example/chatbot/ChatBotApplication.java
@@ -1,13 +1,16 @@
 package com.example.chatbot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatBotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatBotApplication.class, args);
+        SpringApplication app = new SpringApplication(ChatBotApplication.class);
+        app.setWebApplicationType(WebApplicationType.NONE); // 禁用Web容器
+        app.run( args);
     }
 
 }
